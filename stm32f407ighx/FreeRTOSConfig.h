81d9f9b5c459b681ee318eebf15007d0e4a944a6
DuYicheng <duyicheng741@outlook.com>
2025-08-29 21:51:47 +0800

    修改 FreeRTO 配置宏定义，从而正常使用事件组

--- stm32f407ighx/FreeRTOSConfig.h
+++ stm32f407ighx/FreeRTOSConfig.h
@@ -684,7 +684,7 @@ PRIORITY THAN THIS! (higher priorities are lower numeric values. */
 #define INCLUDE_uxTaskGetStackHighWaterMark    0
 #define INCLUDE_xTaskGetIdleTaskHandle         0
 #define INCLUDE_eTaskGetState                  1
-#define INCLUDE_xTimerPendFunctionCall         0
+#define INCLUDE_xTimerPendFunctionCall         1
 #define INCLUDE_xTaskAbortDelay                0
 #define INCLUDE_xTaskGetHandle                 0
 #define INCLUDE_xTaskResumeFromISR             1
