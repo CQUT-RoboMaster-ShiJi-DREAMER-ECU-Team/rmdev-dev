2876e373627a7675914e64dd0ecff8a0cdb7feb6
DuYicheng <duyicheng741@outlook.com>
2025-10-04 18:37:28 +0800

    C板平台增加陀螺仪所需的 SPI 配置

--- Platforms/STM32/RoboMasterDevelopmentBoardTypeC/Core/Src/stm32f4xx_it.c
+++ Platforms/STM32/RoboMasterDevelopmentBoardTypeC/Core/Src/stm32f4xx_it.c
@@ -61,7 +61,6 @@ extern void xPortSysTickHandler(void);
 /* USER CODE END 0 */
 
 /* External variables --------------------------------------------------------*/
-extern UART_HandleTypeDef huart6;
 extern TIM_HandleTypeDef htim7;
 
 /* USER CODE BEGIN EV */
@@ -200,20 +199,6 @@ void TIM7_IRQHandler(void)
   /* USER CODE BEGIN TIM7_IRQn 1 */
 
   /* USER CODE END TIM7_IRQn 1 */
-}
-
-/**
-  * @brief This function handles USART6 global interrupt.
-  */
-void USART6_IRQHandler(void)
-{
-  /* USER CODE BEGIN USART6_IRQn 0 */
-
-  /* USER CODE END USART6_IRQn 0 */
-  HAL_UART_IRQHandler(&huart6);
-  /* USER CODE BEGIN USART6_IRQn 1 */
-
-  /* USER CODE END USART6_IRQn 1 */
 }
 
 /* USER CODE BEGIN 1 */
