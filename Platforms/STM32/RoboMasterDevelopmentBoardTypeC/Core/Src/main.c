31b258b6bc340dbe318a237a391dad0d08d41f62
DuYicheng <duyicheng741@outlook.com>
2025-10-05 19:58:47 +0800

    增加 RoboMaster C 板的定时器配置（用于加热 IMU）

--- Platforms/STM32/RoboMasterDevelopmentBoardTypeC/Core/Src/main.c
+++ Platforms/STM32/RoboMasterDevelopmentBoardTypeC/Core/Src/main.c
@@ -19,6 +19,7 @@
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
 #include "spi.h"
+#include "tim.h"
 #include "usart.h"
 #include "gpio.h"
 
@@ -92,6 +93,7 @@ int main(void)
   MX_GPIO_Init();
   MX_USART6_UART_Init();
   MX_SPI1_Init();
+  MX_TIM10_Init();
   /* USER CODE BEGIN 2 */
 
   testEntry();
