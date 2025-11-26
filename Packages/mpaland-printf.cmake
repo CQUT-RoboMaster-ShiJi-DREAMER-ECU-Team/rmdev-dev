5c4794b59e8f8faa3a5fd11f4c2dabcd5cfdd154
DuYicheng <duyicheng741@outlook.com>
2025-11-26 19:49:52 +0800

    调整 Package 中第三方库的版本；优化 mpaland-printf.cmake 的内容与注释

--- Packages/mpaland-printf.cmake
+++ Packages/mpaland-printf.cmake
@@ -1,6 +1,11 @@
+# 由于 mpaland-printf 没有使用 CMake 构建系统，因此在这里将它作为静态库添加进来
+
 add_library(printf STATIC)
-target_sources(printf PRIVATE Packages/mpaland-printf/printf.c
-    PUBLIC FILE_SET header TYPE HEADERS BASE_DIRS Packages/mpaland-printf
-        FILES Packages/mpaland-printf/printf.h
+target_sources(printf
+    PRIVATE ${CMAKE_CURRENT_LIST_DIR}/mpaland-printf/printf.c
+
+    PUBLIC FILE_SET header TYPE HEADERS
+        BASE_DIRS ${CMAKE_CURRENT_LIST_DIR}/mpaland-printf
+        FILES ${CMAKE_CURRENT_LIST_DIR}/mpaland-printf/printf.h
 )
 target_compile_definitions(printf PRIVATE PRINTF_DISABLE_SUPPORT_EXPONENTIAL)
