238863bacf20cc8959f93472301828bcbf1a7de1
DuYicheng <duyicheng741@outlook.com>
2026-03-11 20:21:44 +0800

    测试适配库提供头文件的情况（demo 提供头文件）

---
+++ RunningPlatforms/STM32/RoboMasterDevelopmentBoardTypeC/Demo/INS_task.hpp
@@ -0,0 +1,10 @@
+/**
+ * @file INS_task.hpp
+ * @brief
+ */
+
+#pragma once
+
+#include "emdevif/core/attributes_and_useful_macros.h"
+
+EMDEVIF_NO_RETURN void insTask(void*);
---
+++ RunningPlatforms/STM32/RoboMasterDevelopmentBoardTypeC/Demo/inc/demo_entry.hpp
@@ -0,0 +1,10 @@
+/**
+ * @file demo_entry.hpp
+ * @brief
+ */
+
+#pragma once
+
+#include "emdevif/core/attributes_and_useful_macros.h"
+
+EMDEVIF_NO_RETURN void demoEntry() noexcept;
