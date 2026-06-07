6d663e43e2141f04743125ee766e7cf5b649835d
DuYicheng <duyicheng741@outlook.com>
2026-06-07 10:46:04 +0800

    增加 CPM 包锁文件的说明；重构 include 的方式

--- cmake/add_packages.cmake
+++ cmake/add_packages.cmake
@@ -1,5 +1,8 @@
 
-include(get_cpm)
+include(${CMAKE_CURRENT_LIST_DIR}/get_cpm.cmake)
+
+# 该工程不应该使用包锁文件，因为该仓库是一系列库的聚合，其子模块有不同的依赖关系，使用包锁文件会导致依赖冲突。
+# CPMUsePackageLock(${CMAKE_CURRENT_LIST_DIR}/package-lock.cmake)
 
 CPMAddPackage(
     NAME printf
@@ -21,7 +24,7 @@ else ()
     message(FATAL_ERROR "[${PROJECT_NAME}]: CPM: Failed to add package `printf`")
 endif ()
 
-CPMAddPackage(
+CPMFindPackage(
     NAME mp-units
     GITHUB_REPOSITORY mpusz/mp-units
     GIT_TAG v2.5.0
